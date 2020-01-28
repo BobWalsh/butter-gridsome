ce8d576c7fd400e0962d4ccf36d433153ae4ae3a
Bob Walsh <bob.walsh@47hats.com>
2020-01-27 18:14:11 -0800

    Updating Index pages

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,6 +6,7 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/studies/">Case Studies</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
@@ -23,9 +24,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
